e86839fcf333ecbad9db45224b30c71a540d9502
pierreFauche <pierre.fauche@gmail.com>
2015-11-01 21:18:53 +0100

    commit2

--- hadoop/src/main/java/com/aamend/hadoop/hadoop/Exo2.java
+++ hadoop/src/main/java/com/aamend/hadoop/hadoop/Exo2.java
@@ -16,100 +16,108 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Exo2 {
 
-public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
+	public static class TokenizerMapper
+    extends Mapper<Object, Text, Text, IntWritable>{
 
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
-        private int i=0;
-        private int j=0;
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
+private final static IntWritable one = new IntWritable(1);
+private Text word = new Text();
 
-            String[] result = value.toString().split(";");
+private int i=0;//nombre d'ami de la personne
+private int j=0;//Age de la personne
 
-            try{
+public void map(Object key, Text value, Context context
+) throws IOException, InterruptedException {
 
-                j=Integer.parseInt(result[2]);
+    String[] result = value.toString().split(";");
 
-                if (j>=0 && j<=5){
-                    word.set("0-5 Years");
-                    i = Integer.parseInt(result[4]);
-                    one.set(i);
-                }
+    try{// on fait un try catch pour éviter une exception pour le header du csv
 
-                if (j>=6 && j<=12){
-                    word.set("6-12 Years");
-                    i = Integer.parseInt(result[4]);
-                    one.set(i);
-                }
+        j=Integer.parseInt(result[2]);//on récupère l'age de la personne pour entrer dans la bonne fourchette
 
-                if (j>=13 && j<=17){
-                    word.set("13-17 Years");
-                    i = Integer.parseInt(result[4]);
-                    one.set(i);
-                }
-
-                if (j>=18 && j<=25){
-                    word.set("18-25 Years");
-                    i = Integer.parseInt(result[4]);
-                    one.set(i);
-                }
-
-                if (j>=26 && j<=35){
-                    word.set("26-35 Years");
-                    i = Integer.parseInt(result[4]);
-                    one.set(i);
-                }
-
-                if (j>=36 && j<=45){
-                    word.set("36-45 Years");
-                    i = Integer.parseInt(result[4]);
-                    one.set(i);
-                }
-
-                if (j>=46 && j<=60){
-                    word.set("46-59 Years");
-                    i = Integer.parseInt(result[4]);
-                    one.set(i);
-                }
-
-                if (j>60){
-                    word.set("60+ Years");
-                    i = Integer.parseInt(result[4]);
-                    one.set(i);
-                }
-
-            }
-            catch(Exception e){
-                i=0;
-                word.set("0");
-                one.set(i);
-            }
-
-            context.write(word, one);
+        if (j>=0 && j<=5){
+            word.set("0-5 Years");
+            i = Integer.parseInt(result[4]);//on prend le nombre d'ami de la personne
+            one.set(i);
         }
+
+        if (j>=6 && j<=12){
+            word.set("6-12 Years");
+            i = Integer.parseInt(result[4]);
+            one.set(i);
+        }
+
+        if (j>=13 && j<=17){
+            word.set("13-17 Years");
+            i = Integer.parseInt(result[4]);
+            one.set(i);
+        }
+
+        if (j>=18 && j<=25){
+            word.set("18-25 Years");
+            i = Integer.parseInt(result[4]);
+            one.set(i);
+        }
+
+        if (j>=26 && j<=35){
+            word.set("26-35 Years");
+            i = Integer.parseInt(result[4]);
+            one.set(i);
+        }
+
+        if (j>=36 && j<=45){
+            word.set("36-45 Years");
+            i = Integer.parseInt(result[4]);
+            one.set(i);
+        }
+
+        if (j>=46 && j<=60){
+            word.set("46-59 Years");
+            i = Integer.parseInt(result[4]);
+            one.set(i);
+        }
+
+        if (j>60){
+            word.set("60+ Years");
+            i = Integer.parseInt(result[4]);
+            one.set(i);
+        }
+
     }
+    catch(Exception e){
+        i=0;
+        word.set("0");
+        one.set(i);
+    }
+
+    context.write(word, one);
+}
+}
+
+
+
+
+
 
 public static class IntSumReducer
-        extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+    extends Reducer<Text,IntWritable,Text,IntWritable> {
+private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-    ) throws IOException, InterruptedException {
-        int sum = 0;
-        int average = 0;
-        int nb=0;
+public void reduce(Text key, Iterable<IntWritable> values,
+                   Context context
+) throws IOException, InterruptedException {
 
-        for (IntWritable val : values) {
-            sum += val.get();
-            nb++;
-        }
-        average = sum / nb;
-        result.set(average);
-        context.write(key, result);
+    int sum = 0;//somme du nombre d'ami par catégorie
+    int average = 0;//moyenne du nombre d'ami
+    int nb=0;//nombre de personne par catégorie
+
+    for (IntWritable val : values) {
+        sum += val.get();
+        nb++;
     }
+    average = sum / nb;
+    result.set(average);
+    context.write(key, result);
+}
 }
 
 
